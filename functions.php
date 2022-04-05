51511ae53838ab18671f01f449c232f6d2d034f3
Roman <roma.alf89@gmail.com>
2022-04-05 23:25:34 +0300

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,19 @@
+//шоркод с курсом евро
+add_shortcode( 'cureuro', 'cureuro_func' );
+
+function cureuro_func($atts){
+	$xml = simplexml_load_file("http://www.cbr.ru/scripts/XML_daily.asp");
+	$percent = "5"; // Процент
+	$cur_data = date("d.m.y");
+
+	foreach ($xml->Valute as $one) {
+		if ($one["ID"] == 'R01239') { //выбираем евро по коду цб
+			$koeficient1 = round(str_replace(',','.',$one->Value), 2); //ее значение
+			$number_percent = $koeficient1 / 100 * $percent;
+			$procent_plus = $koeficient1 + $number_percent;
+			$procent_plus_r =round($procent_plus, 2);
+			$koeficient2 = $one->Nominal.' '.$one->Name.' = '.$procent_plus_r.' руб. от '.$cur_data; //запоминаем номинал
+		} 
+	}
+	return $koeficient2;
+}
