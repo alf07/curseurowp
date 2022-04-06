103d3ae963f68b919e49b6a963cd74c5ed5b1b4c
Roman <roma.alf89@gmail.com>
2022-04-06 09:53:21 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,17 +1,15 @@
-//шоркод с курсом евро
-add_shortcode( 'cureuro', 'cureuro_func' );
 
-function cureuro_func($atts){
+function cureuro_func($atts, $content ){
 	$xml = simplexml_load_file("http://www.cbr.ru/scripts/XML_daily.asp");
 	$percent = "5"; // Процент
 	$cur_data = date("d.m.y");
 
 	foreach ($xml->Valute as $one) {
 		if ($one["ID"] == 'R01239') { //выбираем евро по коду цб
-			$koeficient1 = round(str_replace(',','.',$one->Value), 2); //ее значение
+			$koeficient1 = round(str_replace(',','.',$one->Value), 4); //ее значение
 			$number_percent = $koeficient1 / 100 * $percent;
 			$procent_plus = $koeficient1 + $number_percent;
-			$procent_plus_r =round($procent_plus, 2);
+			$procent_plus_r = round($procent_plus, 4);
 			$koeficient2 = $one->Nominal.' '.$one->Name.' = '.$procent_plus_r.' руб. от '.$cur_data; //запоминаем номинал
 		} 
 	}
